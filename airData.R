397ac63f77817f839f3e9b547c84a1e85b31e264
jmvaldeolmillos <jmanuelv@gmail.com>
2016-05-12 00:01:00 +0200

    modificacion fichero R nombre columnas

--- airData.R
+++ airData.R
@@ -1,5 +1,5 @@
 # configuación de directorio de trabajo
-workingDirectory <- setwd("~/Repos/Proyecto_Master")
+workingDirectory <- setwd("~/Documents/Repos/Proyecto_Master")
 
 # carga de librerias
 library(RSQLite)
@@ -17,42 +17,42 @@ dfEstaciones <- dbGetQuery(con,'select * from estacion')
 dbDisconnect(con)
 
 # cambio tipos de datos en dfHistoricos
-dfHistoricos$Dia <- as.Date(dfHistoricos$Dia)
-dfHistoricos$Provincia <- as.factor(dfHistoricos$Provincia)
+dfHistoricos$date <- as.Date(dfHistoricos$date)
+dfHistoricos$provincia <- as.factor(dfHistoricos$provincia)
 dfHistoricos[,2] <- as.numeric(dfHistoricos[,2])
 dfHistoricos[,3:10] <- sapply(dfHistoricos[,3:10], as.integer)
-dfHistoricos$Provincia <- NULL
-dfHistoricos$Estacion <- as.factor(dfHistoricos$Estacion)
+dfHistoricos$provincia <- NULL
+dfHistoricos$estacion <- as.factor(dfHistoricos$estacion)
 
 # cambio tipos de datos en dfEstaciones
 
-dfEstaciones$Operativa <- as.factor(dfEstaciones$Operativa)
-dfEstaciones$Provincia <- as.factor(dfEstaciones$Provincia)
-dfEstaciones$Altura <- as.integer(dfEstaciones$Altura)
+dfEstaciones$operativa <- as.factor(dfEstaciones$operativa)
+dfEstaciones$provincia <- as.factor(dfEstaciones$provincia)
+dfEstaciones$altura <- as.integer(dfEstaciones$altura)
 
 # preparamos el mapa de estaciones. Pediente para Shiny el filtro de actividad (si o no)
 library(ggmap)
 map <- get_map(location = 'Valladolid', scale=1, zoom = 7, source="google", maptype = "terrain")
-mapPoints <- ggmap(map) + geom_point(aes(x = Longitud, y = Latitud), data = dfEstaciones, color="blue", size=2, alpha = .5)
+mapPoints <- ggmap(map) + geom_point(aes(x = longitud, y = latitud), data = dfEstaciones, color="blue", size=2, alpha = .5)
 mapPoints
 
-# Gráfica de NO2
-hist(dfHistoricos$NO2, main = "Histogram of nitrogen dioxide", xlab = "Nitrogen dioxide (ppb)")
+# Gráfica de no2
+hist(dfHistoricos$no2, main = "Histogram of nitrogen dioxide", xlab = "Nitrogen dioxide (ppb)")
 
-# Gráfica de CO
-hist(dfHistoricos$CO, main = "Histogram of nitrogen dioxide", xlab = "Nitrogen dioxide (ppb)")
+# Gráfica de co
+hist(dfHistoricos$co, main = "Histogram of nitrogen dioxide", xlab = "Nitrogen dioxide (ppb)")
 
-# Gráfica de NO
-hist(dfHistoricos$NO, main = "Histogram of nitrogen oxide", xlab = "Nitrogen oxide (ppb)")
+# Gráfica de no
+hist(dfHistoricos$no, main = "Histogram of nitrogen oxide", xlab = "Nitrogen oxide (ppb)")
 
-# Gráfica de Ozone
-hist(dfHistoricos$Ozono, main = "Histogram of Ozone", xlab = "Ozone")
+# Gráfica de ozone
+hist(dfHistoricos$ozone, main = "Histogram of Ozone", xlab = "Ozone")
 
-# Gráfica de NO2
-hist(dfHistoricos$NO2, main = "Histogram of nitrogen dioxide", xlab = "Nitrogen dioxide (ppb)")
+# Gráfica de so2
+hist(dfHistoricos$so2, main = "Histogram of nitrogen dioxide", xlab = "Nitrogen dioxide (ppb)")
 
-# Gráfica de PM10
-hist(dfHistoricos$PM10, main = "Histogram of PM10", xlab = "PM10")
+# Gráfica de pm10
+hist(dfHistoricos$pm10, main = "Histogram of PM10", xlab = "PM10")
 
-# Gráfica de PM25
-hist(dfHistoricos$PM25, main = "Histogram of PM25", xlab = "PM25")
+# Gráfica de pm25
+hist(dfHistoricos$pm25, main = "Histogram of PM25", xlab = "PM25")
